d20bf25e4465dfd0a44eff6dbb616a8ab8bf5ceb
Philip Tallo <ptallo@Philips-MacBook-Pro.local>
2024-06-14 19:03:10 -0400

    finished initial HeroImage and changed color theme

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,6 @@ function App() {
   return (
     <div>
       <HeroImage/>
-      Hello World! This is a test!
     </div>
   )
 }
--- src/components/HeroImage/HeroImage.tsx
+++ src/components/HeroImage/HeroImage.tsx
@@ -3,18 +3,27 @@ import './HeroImage.scss';
 export function HeroImage() {
   return <div className='hero-image'>
 
-    <div className='intro'>
-      <h1>Hey, I'm Philip Tallo, A fullstack web developer, data scientist, and life long learner.</h1>
+    <div className='content-container'>
+      <div className='intro'>
+        <h1>Hey, I'm Philip Tallo, A <span>web developer</span>, <span>data scientist</span>, and <span>life long learner</span>.</h1>
 
-      <ul>
-        <li>Web Developer</li>
-        <li>Data Scientist</li>
-        <li>Life Long Learner</li>
-      </ul>
+        <ul>
+          <li>Web Developer</li>
+          <li>Data Scientist</li>
+          <li>Life Long Learner</li>
+        </ul>
+      </div>
+
+      <div className='image-container'>
+          <img src="pictures/me-greyscale.png"/>
+      </div>
     </div>
 
-    <div className='image-container'>
-      <img src="me.png"/>
+
+    <div className="triangle-div">
+        <svg data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1200 120" preserveAspectRatio="none">
+            <path d="M892.25 114.72L0 0 0 120 1200 120 1200 0 892.25 114.72z" className="shape-fill"></path>
+        </svg>
     </div>
   </div>;
 }
