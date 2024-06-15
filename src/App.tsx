5cc33541e950332d12e8c33415d83942d3586ae0
Philip Tallo <ptallo@Philips-MacBook-Pro.local>
2024-06-14 20:00:24 -0400

    added navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import './App.scss'
 import { HeroImage } from './components/HeroImage/HeroImage'
+import { Navbar } from './components/Navbar/Navbar'
 
 function App() {
 
   return (
     <div>
+      <Navbar/>
       <HeroImage/>
     </div>
   )
---
+++ src/components/Navbar/Navbar.tsx
@@ -0,0 +1,15 @@
+import './Navbar.scss';
+
+
+export function Navbar() {
+  return <div className='navbar'>
+    <span>PT.</span>
+
+    <ul>
+      <li>Home</li>
+      <li>Web Development</li>
+      <li>Data Science</li>
+      <li>Testamonials</li>
+    </ul>
+  </div>;
+}
