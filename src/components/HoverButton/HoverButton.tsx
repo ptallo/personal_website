20239e6006d78b283df30eca15e2e9e5c86f1b9c
Philip Tallo <philip.tallo@gmail.com>
2024-06-16 04:47:48 -0400

    added button changes

--- src/components/HoverButton/HoverButton.tsx
+++ src/components/HoverButton/HoverButton.tsx
@@ -2,16 +2,12 @@ import './HoverButton.scss';
 
 export interface HoverButtonProps {
   text?: string;
-  height?: number;
-  width?: number;
 }
 
 export function HoverButton({
   text = 'Something',
-  height = 60,
-  width = 320,
 }: HoverButtonProps) {
-  return <div className='hover-button'>
+  return <button className="hover-button">
     <span>{text}</span>
-  </div>
+  </button>
 }
--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,27 +1,20 @@
 import { HoverButton } from '../HoverButton/HoverButton';
 import './Navbar.scss';
 
-let buttonOptions = {
-  height: 60,
-  width: 320,
-  ids: [
-    "Home",
-    "Web Development",
-    "Data Science",
-    "Testamonials",
-  ],
-}
+let ids = [
+  "Home",
+  "Web Development",
+  "Data Science",
+  "Testamonials",
+]
+
 
 export function Navbar() {
   return <div className='navbar'>
     <span className='initials'>PT.</span>
 
     <div className='flex-container'>
-      {buttonOptions.ids.map(t => <HoverButton 
-        text={t} 
-        height={buttonOptions.height} 
-        width={buttonOptions.width}
-      />)}
+      {ids.map(t => <HoverButton text={t} />)}
     </div>
   </div>;
 }
