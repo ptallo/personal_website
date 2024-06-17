202124d5678cd417676cc8c39cdcb3cc746ba1ed
Philip Tallo <ptallo@Philips-MBP.home>
2024-06-16 20:13:08 -0400

    added fontawesome icons for github and linkedin

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,20 +1,22 @@
-import { HoverButton } from '../HoverButton/HoverButton';
 import './Navbar.scss';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faLinkedin, faGithub } from '@fortawesome/free-brands-svg-icons';
 
-let ids = [
-  "Home",
-  "Web Development",
-  "Data Science",
-  "Testamonials",
-]
-
+const linkedInLink : string = 'https://www.linkedin.com/in/philip-tallo-444667107/';
+const githubLink : string = 'https://github.com/ptallo';
 
 export function Navbar() {
   return <div className='navbar'>
     <span className='initials'>PT.</span>
 
     <div className='flex-container'>
-      {ids.map(t => <HoverButton text={t} />)}
+      <a href=''>Home</a>
+      <a href=''>Web Development</a>
+      <a href=''>Data Science</a>
+      <a href=''>Testamonials</a>
+      |
+      <a href={linkedInLink}><FontAwesomeIcon icon={faLinkedin} size='xl'/></a>
+      <a href={githubLink}><FontAwesomeIcon icon={faGithub} size='xl'/></a>
     </div>
   </div>;
 }
