2cd0f220b6d36430b7278d59fff15a541dc18b9e
Philip Tallo <philip.tallo@gmail.com>
2024-06-18 22:28:56 -0400

    finished adding skills to skills section

--- src/components/SkillsSection/SkillsSection.tsx
+++ src/components/SkillsSection/SkillsSection.tsx
@@ -1,7 +1,7 @@
 import './SkillsSection.scss';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faAngular, faDocker, faPython, faReact } from '@fortawesome/free-brands-svg-icons';
-import { faLanguage } from '@fortawesome/free-solid-svg-icons';
+import { faAngular, faDocker, faPython, faReact, faGoogle } from '@fortawesome/free-brands-svg-icons';
+import { faLanguage, faRobot, faDatabase, faFlaskVial } from '@fortawesome/free-solid-svg-icons';
 
 
 export function SkillsSection() {
@@ -37,28 +37,33 @@ export function SkillsSection() {
 
       <div className="card">
         <FontAwesomeIcon className="icon" icon={faLanguage} />
-        <h1>NLP</h1>
-        <p></p>
+        <h1>Natural Language Processing</h1>
+        <p>I completed my Master's of Computer Science with a focus in NLP in 2022. Since then it has been a pleasure to watch NLP hit the mainstream!</p>
       </div>
 
       <div className="card">
-        <FontAwesomeIcon className="icon" icon={faAngular} />
-        <h1>Angular</h1>
-        <p>I have extensive experience developing enterprise Angular applications which I gained while working for Aptera! </p>
+        <FontAwesomeIcon className="icon" icon={faRobot} />
+        <h1>Generative AI</h1>
+        <p>Since I studied this in my master's degree I am uniquely position to take advantage of advances, but also understand weaknesses in Generative AI!</p>
       </div>
 
       <div className="card">
-        <FontAwesomeIcon className="icon" icon={faAngular} />
-        <h1>Angular</h1>
-        <p>I have extensive experience developing enterprise Angular applications which I gained while working for Aptera! </p>
+        <FontAwesomeIcon className="icon" icon={faGoogle} />
+        <h1>Google Cloud Platform</h1>
+        <p>For several years now I have had the pleasure of working with many aspects of GCP. Including but not limited to: Cloud Run, Looker, Big Query, Cloud Storage, Container Registry and Kubernetes Engine!</p>
       </div>
 
       <div className="card">
-        <FontAwesomeIcon className="icon" icon={faAngular} />
-        <h1>Angular</h1>
-        <p>I have extensive experience developing enterprise Angular applications which I gained while working for Aptera! </p>
+        <FontAwesomeIcon className="icon" icon={faDatabase} />
+        <h1>Entity Framwork</h1>
+        <p>While developing enterprise applications during my consulting work at Aptera, Entity Framwork was a key component of our architecture. We utilized it's code driven ORM to quickly develop new features!</p>
       </div>
 
+      <div className="card">
+        <FontAwesomeIcon className="icon" icon={faFlaskVial} />
+        <h1>Test Driven Development</h1>
+        <p>Using TDD for any serious application is a must! It's hard to pass up either the efficient coding it enforces or the feature regression it protects against!</p>
+      </div>
     </div>
   </div>;
 }
