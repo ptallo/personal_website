c72596d44bd8f36271548fd4ca0aa626705d0853
Philip Tallo <philip.tallo@gmail.com>
2024-06-24 22:40:53 -0400

    modified styling to be more consistant in size and width

--- src/components/SkillsSection/SkillsSection.tsx
+++ src/components/SkillsSection/SkillsSection.tsx
@@ -1,7 +1,7 @@
 import './SkillsSection.scss';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faAngular, faDocker, faPython, faReact, faGoogle } from '@fortawesome/free-brands-svg-icons';
-import { faLanguage, faRobot, faDatabase, faFlaskVial, faCheck } from '@fortawesome/free-solid-svg-icons';
+import { faLanguage, faRobot, faDatabase, faFlask, faVial, faCheck } from '@fortawesome/free-solid-svg-icons';
 import { CircleProgressBar } from '../CircleProgressBar/CircleProgressBar';
 
 
@@ -13,20 +13,21 @@ export function SkillsSection() {
       <ul>
         <li><FontAwesomeIcon className='check' icon={faCheck}/>Designing clean experiences for the web!</li>
         <li><FontAwesomeIcon className='check' icon={faCheck}/>Building highly performant models!</li>
-        <li><FontAwesomeIcon className='check' icon={faCheck}/>Deploying it all to the cloud!</li>
+        <li><FontAwesomeIcon className='check' icon={faCheck}/>Continuously deploying it all to the cloud!</li>
       </ul>
     </div>
 
     <div className="cards">
       <CircleProgressBar text="Angular" icon={faAngular} progress={80} />
       <CircleProgressBar text="React" icon={faReact} progress={60} />
-      <CircleProgressBar text="Docker" icon={faDocker} progress={70} />
+      <CircleProgressBar text="Docker" icon={faDocker} progress={80} />
       <CircleProgressBar text="Python" icon={faPython} progress={90} />
       <CircleProgressBar text="Natural Language Processing" icon={faLanguage} progress={90} />
       <CircleProgressBar text="Generative AI" icon={faRobot} progress={70} />
-      <CircleProgressBar text="Google Cloud Platform" icon={faGoogle} progress={60} />
+      <CircleProgressBar text="Google Cloud Platform" icon={faGoogle} progress={70} />
       <CircleProgressBar text="Entity Framework" icon={faDatabase} progress={60} />
-      <CircleProgressBar text="Test Driven Development" icon={faFlaskVial} progress={70} />
+      <CircleProgressBar text="Test Driven Development" icon={faFlask} progress={70} />
+      <CircleProgressBar text="Experiment Design" icon={faVial} progress={80} />
     </div>
 
   </div>;
