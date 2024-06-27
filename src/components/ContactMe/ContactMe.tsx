461f3c7bb70c6d7e873a35ef3187841b65f1d7cb
Philip Tallo <ptallo@Philips-MacBook-Pro.local>
2024-06-27 19:24:24 -0400

    some changes to contact me

--- src/components/ContactMe/ContactMe.tsx
+++ src/components/ContactMe/ContactMe.tsx
@@ -1,22 +1,68 @@
 import { useState } from 'react';
 import './ContactMe.scss';
 
+interface InputsInterface {
+  firstname: string;
+  lastname: string;
+  email: string;
+  reason: string;
+}
 
 export function ContactMe() {
-  const [name, setName] = useState("");
+  const [inputs, setInputs]: [InputsInterface, Function] = useState({
+    'firstname': '',
+    'lastname': '',
+    'email': '',
+    'reason': '',
+  });
+
+  const handleChange = (event: React.FormEvent<HTMLInputElement>) => {
+    const name = event.currentTarget.name;
+    const value = event.currentTarget.value;
+    setInputs((values: InputsInterface) => ({ ...values, [name]: value }))
+  }
 
   const handleSubmit = (event: React.FormEvent) => {
     event.preventDefault();
-    alert()
+    alert(inputs);
   }
 
-  return <div className='contact-me'>
-    <h1>Contact Me</h1>
-    <form onSubmit={handleSubmit}>
-      <label>Firstname
-        <input type="text" value={name} onChange={(e) => setName(e.target.value)}/>
-      </label>
-      <input type="submit" />
-    </form>
-  </div>;
+  return (
+    <div className='contact-me'>
+      <h1>Contact Me</h1>
+      <form onSubmit={handleSubmit}>
+
+        <input
+          type="text"
+          name="firstname"
+          value={inputs.firstname || ""}
+          onChange={handleChange}
+        />
+
+        <input
+          type="text"
+          name="lastname"
+          value={inputs.lastname || ""}
+          onChange={handleChange}
+        />
+        
+        <input
+          type="email"
+          name="email"
+          value={inputs.email || ""}
+          onChange={handleChange}
+        />
+
+        <input
+          type="text"
+          name="reason"
+          value={inputs.reason || ""}
+          onChange={handleChange}
+        />
+
+        <input type="submit" />
+      </form>
+
+    </div>
+  )
 }
