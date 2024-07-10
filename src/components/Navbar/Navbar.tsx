760ce226a58cb3d421c45aaa7efa787a381932ae
Philip Tallo <philip.tallo@gmail.com>
2024-07-10 14:12:53 -0400

    updated scrolling a bit

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,22 +1,31 @@
 import './Navbar.scss';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faLinkedin, faGithub } from '@fortawesome/free-brands-svg-icons';
+import { MouseEventHandler, useRef } from 'react';
 
-const linkedinLink : string = 'https://www.linkedin.com/in/philip-tallo-444667107/';
-const githubLink : string = 'https://github.com/ptallo';
+const linkedinLink: string = 'https://www.linkedin.com/in/philip-tallo-444667107/';
+const githubLink: string = 'https://github.com/ptallo';
 
 export function Navbar() {
+  const scrollToClass = (className: string) => {
+    document.getElementsByClassName(className).item(0)?.scrollIntoView({ 
+      behavior: 'smooth',
+      block: 'start',
+      inline: 'nearest',
+    });
+  }
+
   return <div className='navbar'>
     <span className='initials'>PT.</span>
 
     <div className='flex-container'>
-      <a href=''>Home</a>
-      <a href=''>Skills</a>
-      <a href=''>Employment</a>
-      <a href=''>Contact Me</a>
-      <a href=''>|</a>
-      <a href={linkedinLink} target='_blank'><FontAwesomeIcon icon={faLinkedin} size='xl'/></a>
-      <a href={githubLink} target='_blank'><FontAwesomeIcon icon={faGithub} size='xl'/></a>
+      {/* <div className='' onClick={scrollToElement}>Home</div> */}
+      <div className='to-hide' onClick={(e) => { scrollToClass('skills') }}>Skills</div>
+      <div className='to-hide' onClick={(e) => { scrollToClass('employment-history') }}>Employment</div>
+      <div className='to-hide' onClick={(e) => { scrollToClass('contact-me') }}>Contact Me</div>
+      <div className='to-hide' >|</div>
+      <a href={linkedinLink} target='_blank'><FontAwesomeIcon icon={faLinkedin} size='xl' /></a>
+      <a href={githubLink} target='_blank'><FontAwesomeIcon icon={faGithub} size='xl' /></a>
     </div>
   </div>;
 }
