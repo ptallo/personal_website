e1fca73d3270c3ddeff7e73ba1ab588037d70721
Philip Tallo <ptallo@users.noreply.github.com>
2026-04-02 15:20:23 -0400

    updated allowedHosts to be a list and removed unecessary env variables

--- vite.config.ts
+++ vite.config.ts
@@ -2,21 +2,21 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 const defaultPort = 8080;
-const allowedHosts = (process.env.VITE_ALLOWED_HOSTS || 'localhost').split(',').map(h => h.trim())
+const allowedHosts = process.env.VITE_ALLOWED_HOSTS ? JSON.parse(process.env.VITE_ALLOWED_HOSTS) : ['localhost'];
 
 export default defineConfig({
   base: "/",
   plugins: [react()],
   preview: {
-    port: process.env.VITE_PORT ? parseInt(process.env.VITE_PORT) : defaultPort,
+    port: defaultPort,
     strictPort: true,
     allowedHosts: allowedHosts,
   },
   server: {
-    port: process.env.VITE_PORT ? parseInt(process.env.VITE_PORT) : defaultPort,
+    port: defaultPort,
     strictPort: true,
     host: true,
-    origin: process.env.VITE_SERVER_ORIGIN || "http://localhost:8080",
+    origin: "http://localhost:8080",
     allowedHosts: allowedHosts,
   },
 });
